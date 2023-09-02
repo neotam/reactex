2c0dec6428c8a8f176d3a1fbe575bb362dc31dc8
Naveen T <me@neot.am>
2023-09-03 02:29:11 +0530

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  
+
   // Mode tells the webpack whether to use built-in optmization, default is to production
   // It cam be this.development, proeuction or none  
 
@@ -15,40 +15,34 @@ module.exports = {
     chunkFilename: '[name].js'
   },
 
-  // Defining the rules for webpack to pick up the 
-  // source files to compile using specified transpilers or compilers 
-  // And, loader describes to webpack, how to compile when it comes across require()/import statements before
-  // including in build 
-  module: {                  
-    rules: [{               
-      test: /.jsx?$/,
-      include: [
-        path.resolve(__dirname, 'app')
-      ],
-      exclude: [
-        path.resolve(__dirname, 'node_modules')
-      ],
-      loader: 'babel-loader',
-      options: {
-        presets: [
-          ["@babel/env", {
-            "targets": {"browsers": "last 2 chrome versions"}
-          }, "@babel/preset-react"]
-        ]
-      }
-    }]
-  },
+
+
   resolve: {
-    extensions: ['.json', '.js', '.jsx']    
+    extensions: ['.json', '.js', '.jsx']
   },
   devtool: 'source-map',
   devServer: {
     host: 'localhost',
-    port: 8080,
-    static: ["./public"], 
-    
+    port: 5053,
+    static: ["./public"],
+
     open: true,  //Open the browser after succesfully started
     hot: false,    // Hot realoading and HMR (Hot Module Replacement), hot reload refreshes only files changed whtiout losing app state
     liveReload: true  // Disable or enable live reload on the browser. "hot" must be false for this to take effect 
+  },
+
+  // Defining the rules for webpack to pick up the 
+  // source files to compile using specified transpilers or compilers 
+  // And, loader describes to webpack, how to compile when it comes across require()/import statements before
+  // including in build 
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/, 
+        use: 'babel-loader', 
+        exclude: [path.resolve(__dirname, 'node_modules')]
+      }
+    ]
   }
+
 };
